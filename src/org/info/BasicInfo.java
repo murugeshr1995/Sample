0d96195ee2bb8974730158adeeeeaffeef830230
murugeshr1995 <rcmurugesh.95@gmail.com>
2022-09-22 16:41:39 +0530

    new code added

---
+++ src/org/info/BasicInfo.java
@@ -0,0 +1,21 @@
+package org.info;
+
+public class BasicInfo {
+	private void namE() {
+		System.out.println("murugesh");
+
+	}
+private void address() {
+	System.out.println("chennai");
+
+}
+public static void main(String[] args) {
+	BasicInfo a=new BasicInfo();
+	Bdt b=new Bdt();
+	a.namE();
+	a.address();
+	b.phn();
+}
+
+
+}
---
+++ src/org/info/Bdt.java
@@ -0,0 +1,10 @@
+package org.info;
+
+public class Bdt {
+public void phn() {
+	System.out.println("987544644");
+
+}
+
+
+}
